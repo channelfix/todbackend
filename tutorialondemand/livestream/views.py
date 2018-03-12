8b8289b02f0cc36efc22669be6e3106e6904ac7e
pulmonesxavier <pulmonesxavier@gmail.com>
2018-03-12 19:21:05 +0800

    Initial commit

--- tutorialondemand/livestream/views.py
+++ tutorialondemand/livestream/views.py
@@ -2,28 +2,27 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from livestream.models import Opentok
 from livestream.serializers import OpentokSerializer
 from livestream.utils import OpenTokResource
+from rest_framework.views import APIView
 
 
 # Create your views here.
 class SessionView(APIView):
 
-    def get(self, request, format=None):
+    def get(self, request):
+
         opentok = OpenTokResource.get_client()
         session = opentok.create_session()
         token = opentok.generate_token(session.session_id)
         api_key = settings.APIKEY
-        opentok = Opentok(
-            access_token=token,
-            session_id=session.session_id,
-            api_key=api_key
-        )
-        opentok.save()
-
-        serializer = OpentokSerializer(opentok)
-
+        opentok = {'access_token': token,
+                   'session_id': session.session_id,
+                   'api_key': api_key
+                   }
+        serializer = OpentokSerializer(data=opentok)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
